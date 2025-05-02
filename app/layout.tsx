7a0b1027137d312cb25fb1951aab25e16861dadf
DESKTOP-2695L0M\Rajesh <rajesh147nkp@gmail.com>
2025-05-02 22:27:40 +0530

    remove the build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mazhilunthu Valaithalam",
   description: "A Tamil Valaithalam for Mazhilunthu",
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} antialiased`}>
+        <body className={`antialiased`}>
           <Header />
           <main className="min-h-screen ">{children}</main>
           <Toaster richColors/>
